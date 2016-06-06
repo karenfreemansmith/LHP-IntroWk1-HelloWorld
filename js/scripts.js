ac492178d5436a2f93477d3f446c818058bcd15e
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-06-05 19:19:22 -0700

    replace hand-coded language phrases with API generated phrases

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+//load data into page from api
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if(xhr.readyState===4) {
+    var languages = JSON.parse(xhr.responseText);
+    var linkHTML = "<ul>";
+    languages.forEach(function(language) {
+      linkHTML += "<li><strong>"+language.language+":</strong> "+language.hello_world+"</li>";
+    });
+    linkHTML += "</ul>";
+    document.getElementById("languages").innerHTML=linkHTML;
+  }
+};
+xhr.open('GET', 'api/languages.json');
+xhr.send();
